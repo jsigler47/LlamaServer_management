ff54138d1bcd08b7f2c3ed59259cb8ffdb374ac0
Dylan Sigler <jsigler47@gmail.com>
2021-01-23 16:23:31 -0800

    Converted for use on Ubuntu 18.04 Workstation

--- run_playbook.sh
+++ run_playbook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh-copy-id -f root@192.168.1.10
-ansible-playbook -vv -i hosts.ini site.yml -b -u root
+# ssh-copy-id -f root@192.168.1.10
+sudo ansible-playbook -vv -i hosts.ini site.yml -b -u root --check
